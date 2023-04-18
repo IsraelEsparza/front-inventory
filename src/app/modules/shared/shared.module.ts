49e8565b6179ce90b3592da75eba2a837341a0b5
IsraelEsparza <isra912747@gmail.com>
2023-04-17 22:53:12 -0700

    Sidenav & Menu

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    SidenavComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SidenavComponent],
+  exports: [SidenavComponent],
+  imports: [CommonModule, FlexLayoutModule, MaterialModule, RouterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
