c230b7c36adadb40d1e16d97ae355ea5c4eecfc6
IsraelEsparza <isra912747@gmail.com>
2023-04-18 23:39:46 -0700

    Dialog Guardar
    Guardar Categoria

--- src/app/modules/category/category.module.ts
+++ src/app/modules/category/category.module.ts
@@ -4,10 +4,12 @@ import { MaterialModule } from '../shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CategoryComponent } from './components/category/category.component';
+import { NewCategoryComponent } from './components/new-category/new-category.component';
 
 @NgModule({
   declarations: [
-    CategoryComponent
+    CategoryComponent,
+    NewCategoryComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from 'src/app/modules/shared/services/category.service';
+import { NewCategoryComponent } from '../new-category/new-category.component';
+import {
+  MatSnackBar,
+  MatSnackBarRef,
+  SimpleSnackBar,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-category',
@@ -8,7 +15,11 @@ import { CategoryService } from 'src/app/modules/shared/services/category.servic
   styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-  constructor(private categoryService: CategoryService) {}
+  constructor(
+    private categoryService: CategoryService,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.getCategories();
@@ -38,6 +49,30 @@ export class CategoryComponent implements OnInit {
         this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
       });
     }
+  }
+
+  openCategoryDialog() {
+    const dialogRef = this.dialog.open(NewCategoryComponent, {
+      width: '450px',
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result == 1) {
+        this.openSnackbar('Categoria Agregada', 'Exitosa');
+        this.getCategories();
+      } else if (result == 2) {
+        this.openSnackbar('Se produjo un error al guardar Categoria', 'Error');
+      }
+    });
+  }
+
+  openSnackbar(
+    message: string,
+    actions: string
+  ): MatSnackBarRef<SimpleSnackBar> {
+    return this.snackBar.open(message, actions, {
+      duration: 2000,
+    });
   }
 }
 
